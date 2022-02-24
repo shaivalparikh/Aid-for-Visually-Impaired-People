3f97ce9eba9142c8de086f85c3a72dc49d9121df
Shaival Parikh <38051603+shaivalparikh@users.noreply.github.com>
2022-02-23 20:13:32 -0500

    Add files via upload

---
+++ multi_thread_image.py
@@ -0,0 +1,83 @@
+import threading
+import cv2
+import time
+import numpy as np
+
+start = time.perf_counter()
+
+
+def camera(z):
+    video = cv2.VideoCapture(z)
+
+    check, img = video.read()
+
+
+def image():
+    img = cv2.resize(img, None, fx=0.8, fy=0.8)
+    height, width, channels = img.shape
+    # Detecting objects
+    bifurcation_x = int(width / 5)
+    crop_img = img[:, bifurcation_x:4 * bifurcation_x]
+    img = crop_img
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+    # Showing informations on the screen
+
+
+def object_detect(img,outs,width,height):
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
+    cv2.imshow("Image {}".format(z), img)
+    end = time.perf_counter()
+    print(end - start)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+class_ids = []
+confidences = []
+boxes = []
+
+t1 = threading.Thread(target=camera, args=[0])
+t2 = threading.Thread(target=image)
+t3 = threading.Thread(target=object_detect)
+
+t1.start()
+t2.start()
+t3.start()
+
+t1.join()
+t2.join()
+t3.join()
